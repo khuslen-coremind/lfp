14c7618070513316b12ff694e5b4c590e1015241
khuslen-bumanit <khuslen@bumanit.mn>
2022-03-24 23:00:04 +0800

    .

--- src/components/room/CreateRoom.js
+++ src/components/room/CreateRoom.js
@@ -1,9 +1,12 @@
 import { Text, Group, Button, createStyles } from "@mantine/core";
 // import { GasStation, Gauge, ManualGearbox, Users } from "tabler-icons-react";
 import { BsPeople } from "react-icons/bs";
+import { IconContext } from "react-icons/lib";
 const useStyles = createStyles((theme) => ({
 	button: {
-		background: "#A001",
+		border: "1px solid #e9ecef",
+		background: "#fff",
+
 		borderRadius: 4,
 	},
 }));
@@ -11,9 +14,21 @@ const useStyles = createStyles((theme) => ({
 function CreateRoom(props) {
 	const { classes } = useStyles();
 	return (
-		<Group position="apart" p="xs" className={classes.button}>
-			<Text>Create a waiting room for your party</Text>
-			<Button rightIcon={<BsPeople />}>MAKE A ROOM</Button>
+		<Group position="apart" p="xs" className={classes.button} pr={14} pl={29}>
+			<Text size={15} mb={2}>
+				Create a waiting room for your party
+			</Text>
+			<Button
+				px={13}
+				pr={14}
+				rightIcon={
+					<IconContext.Provider
+						value={{ style: { marginLeft: 0, fontSize: 15, marginBottom: -1 } }}>
+						<BsPeople />
+					</IconContext.Provider>
+				}>
+				MAKE A ROOM
+			</Button>
 		</Group>
 	);
 }
